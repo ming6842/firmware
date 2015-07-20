65dd9311c14c51b25d94edd7d17ccec1c5569e07
ming6842 <ming6842@hotmail.com>
2015-07-20 18:21:52 +0800

    Added hardware abstraction of LED

--- program/mcu_periph/led.h
+++ program/mcu_periph/led.h
@@ -7,11 +7,29 @@
 #define LED_ON(led_name)  GPIO_ResetBits(led_name)
 #define LED_OFF(led_name)  GPIO_SetBits(led_name)
 
-#define LED1 GPIOE, GPIO_Pin_8
-#define LED2 GPIOE, GPIO_Pin_10
-#define LED3 GPIOE, GPIO_Pin_12
-#define LED4 GPIOE, GPIO_Pin_15
-#define TOGGLE_DEBUG GPIOC, GPIO_Pin_9
+
+	
+#ifdef configBOARD_VERTIGO_V20
+
+	#define LED1 GPIOE, GPIO_Pin_8
+	#define LED2 GPIOE, GPIO_Pin_10
+	#define LED3 GPIOE, GPIO_Pin_12
+	#define LED4 GPIOE, GPIO_Pin_15
+	#define TOGGLE_DEBUG GPIOC, GPIO_Pin_9
+
+#endif
+
+
+	
+#ifdef configBOARD_VERTIGO_V21
+
+	#define LED1 GPIOD, GPIO_Pin_10
+	#define LED2 GPIOE, GPIO_Pin_10
+	#define LED3 GPIOE, GPIO_Pin_12
+	#define LED4 GPIOE, GPIO_Pin_15
+	#define TOGGLE_DEBUG GPIOC, GPIO_Pin_9
+
+#endif
 
 void cycle_led(uint8_t );
 
--- program/vertigo_v2_config.h
+++ program/vertigo_v2_config.h
@@ -1,33 +1,79 @@
-//Vertigo v2.0 board setting
-#define USE_FUTABA 
-#define USE_IMU_MPU9250
-#define USE_MAG_HMC5983
-#define USE_CAN_MAGNETOMETER
-#define USE_ADS1246_MPX6115A
-//#define USE_CAN_ADS1246_MPX6115A 
-#define STM32F427X
-/*choose the external device*/
-#define IS_USE_MAGNETOMETER 1
-#define IS_USE_BAROMETER 1
-#define IS_USE_GPS 1
-/*Set magnetometer*/
-#if IS_USE_MAGNETOMETER
 
+/* Define board selection */
+/* Currently support 
+
+	configBOARD_VERTIGO_V20 : Vertigo Control V2.0
+	configBOARD_VERTIGO_V21 : Vertigo Control V2.1
+	*/
+#define configBOARD_VERTIGO_V21
+
+
+#ifdef configBOARD_VERTIGO_V20
+	//Vertigo v2.0 board setting
+	#define USE_FUTABA 
+	#define USE_IMU_MPU9250
+	#define USE_MAG_HMC5983
+	#define USE_CAN_MAGNETOMETER
+	#define USE_ADS1246_MPX6115A
+	//#define USE_CAN_ADS1246_MPX6115A 
+	#define STM32F427X
+	/*choose the external device*/
+	#define IS_USE_MAGNETOMETER 1
+	#define IS_USE_BAROMETER 1
+	#define IS_USE_GPS 1
+	/*Set magnetometer*/
+	#if IS_USE_MAGNETOMETER
+
+	#endif
+	/*Set barometer*/
+	#if IS_USE_BAROMETER
+
+	#endif
+
+	/*Set GPS*/
+	#if IS_USE_GPS
+
+	#endif
+
+	// Controller define
+
+	#define CONTROL_f (4000.0f)
+	#define CONTROL_DT 0.00025f
+	#define USE_MAGNETIC_HEADING
+	//#define USE_GYROSCOPE_HEADING
 #endif
-/*Set barometer*/
-#if IS_USE_BAROMETER
 
+#ifdef configBOARD_VERTIGO_V21
+	//Vertigo v2.0 board setting
+	#define USE_FUTABA 
+	#define USE_IMU_MPU9250
+	#define USE_MAG_HMC5983
+	#define USE_CAN_MAGNETOMETER
+	#define USE_ADS1246_MPX6115A
+	//#define USE_CAN_ADS1246_MPX6115A 
+	#define STM32F427X
+	/*choose the external device*/
+	#define IS_USE_MAGNETOMETER 1
+	#define IS_USE_BAROMETER 1
+	#define IS_USE_GPS 1
+	/*Set magnetometer*/
+	#if IS_USE_MAGNETOMETER
+
+	#endif
+	/*Set barometer*/
+	#if IS_USE_BAROMETER
+
+	#endif
+
+	/*Set GPS*/
+	#if IS_USE_GPS
+
+	#endif
+
+	// Controller define
+
+	#define CONTROL_f (4000.0f)
+	#define CONTROL_DT 0.00025f
+	#define USE_MAGNETIC_HEADING
+	//#define USE_GYROSCOPE_HEADING
 #endif
-
-/*Set GPS*/
-#if IS_USE_GPS
-
-#endif
-
-// Controller define
-
-#define CONTROL_f (4000.0f)
-#define CONTROL_DT 0.00025f
-#define USE_MAGNETIC_HEADING
-//#define USE_GYROSCOPE_HEADING
-
